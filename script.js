62dfb85d3b1625c4e71c29c6f824d2dd4772ac2b
JuliaGabrielaCavalcanti <168544893+JuliaGabrielaCavalcanti@users.noreply.github.com>
2025-09-15 11:05:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ document.getElementById('game1-btn').addEventListener('click', function() {
 
 document.getElementById('game2-btn').addEventListener('click', function() {
     // Redireciona para o Jogo 2 (GitHub Pages)
-    window.location.href = 'https://seu-usuario.github.io/seu-repositorio/jogo2.html'; 
+    window.location.href = 'https://juliagabrielacavalcanti.github.io/jogodavelha/jogodavelha.html'; 
 });
 
 document.getElementById('game3-btn').addEventListener('click', function() {
     // Redireciona para o Jogo 3 (GitHub Pages)
     window.location.href = 'https://seu-usuario.github.io/seu-repositorio/jogo3.html'; 
 });
+
