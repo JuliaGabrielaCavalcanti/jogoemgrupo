d145dcf0598e98c223ccc112557a5379aa5b59eb
JuliaGabrielaCavalcanti <168544893+JuliaGabrielaCavalcanti@users.noreply.github.com>
2025-09-16 09:49:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('game1-btn').addEventListener('click', function() {
     // Redireciona para o Jogo 1 (GitHub Pages)
-    window.location.href = 'https://seu-usuario.github.io/seu-repositorio/jogo1.html'; 
+    window.location.href = 'https://mirellaribeiro30.github.io/3Jogos/falling.html'; 
 });
 
 document.getElementById('game2-btn').addEventListener('click', function() {
@@ -12,6 +12,7 @@ document.getElementById('game3-btn').addEventListener('click', function() {
     // Redireciona para o Jogo 3 (GitHub Pages)
     window.location.href = 'https://acsa3103.github.io/Jogo-Snake/JogoSnake.html'; 
 });
+
 
 
 
